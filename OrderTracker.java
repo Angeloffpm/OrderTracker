7866e437b5968c1d1d2124a18355752d74d3e77f
Pierce Michael Angeloff <angeloffpm@s.dcsdk12.org>
2020-01-10 08:25:06 -0700

    Added bananas

--- OrderTracker.java
+++ OrderTracker.java
@@ -5,7 +5,8 @@ public class OrderTracker {
         Customer customer1 = new Customer("Bob", "bob@gmail.com");
         Item item1 = new Item("Oranges", 11111, 500);
         Item item2 = new Item("Apples", 11112, 4010);
-        Item[] itemList = {item1, item2};
+        Item item3 = new Item("Bananas", 11113, 1000);
+        Item[] itemList = {item1, item2, item3};
         Order order1 = new Order(itemList, customer1);
 
         System.out.println(order1.totalPrice());
